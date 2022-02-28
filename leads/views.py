3f88c41ec54359c20367c775fc76a68b410d370f
Jisoo <jisoooh0202@gmail.com>
2022-02-28 13:34:53 +0700

    model forms and make model simpler

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Lead, Agent
-from .forms import LeadForm
+from .forms import LeadForm, LeadModelForm
 
 
 def lead_list(request):
@@ -20,22 +20,36 @@ def lead_detail(request, pk):
 	return render(request, "lead_detail.html", context)
 	
 def lead_create(request):
-	form = LeadForm()
+	form = LeadModelForm()
 	if request.method == "POST":
-		form = LeadForm(request.POST)
+		form = LeadModelForm(request.POST)
 		if form.is_valid():
-			first_name = form.cleaned_data['first_name']
-			last_name = form.cleaned_data['last_name']
-			age = form.cleaned_data['age']
-			agent = Agent.objects.first()
-			Lead.objects.create(
-				first_name=first_name,
-				last_name=last_name,
-				age=age,
-				agent=agent
-			)
+			form.save()
 			return redirect("/leads")
 	context = {
 		"form": form
 	}
 	return render(request, "lead_create.html", context)
+
+# below code is the reference of the above
+
+# def lead_create(request):
+# 	form = LeadForm()
+# 	if request.method == "POST":
+# 		form = LeadForm(request.POST)
+# 		if form.is_valid():
+# 			first_name = form.cleaned_data['first_name']
+# 			last_name = form.cleaned_data['last_name']
+# 			age = form.cleaned_data['age']
+# 			agent = Agent.objects.first()
+# 			Lead.objects.create(
+# 				first_name=first_name,
+# 				last_name=last_name,
+# 				age=age,
+# 				agent=agent
+# 			)
+# 			return redirect("/leads")
+# 	context = {
+# 		"form": form
+# 	}
+# 	return render(request, "lead_create.html", context)
