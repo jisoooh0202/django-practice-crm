4d76c422610e8cfa51443ff630f565d0340d6233
Jisoo <jisoooh0202@gmail.com>
2022-03-09 10:18:47 +0700

    add url names

--- leads/urls.py
+++ leads/urls.py
@@ -5,9 +5,9 @@ from .views import lead_list, lead_detail, lead_create, lead_update, lead_delete
 app_name = "leads"
 
 urlpatterns = [
-	path('', lead_list),
-	path('create/', lead_create),
-	path('<pk>/', lead_detail),
-	path('<pk>/update/', lead_update),
-	path('<pk>/delete/', lead_delete)
+	path('', lead_list, name='lead-list'),
+	path('create/', lead_create, name='lead-create'),
+	path('<pk>/', lead_detail, name='lead-detail'),
+	path('<pk>/update/', lead_update, name='lead-update'),
+	path('<pk>/delete/', lead_delete, name='lead-delete')
 ]
