33664ed5080cd6745f140edd7b0c627fa9dedf1c
Jisoo <jisoooh0202@gmail.com>
2022-03-10 16:17:06 +0700

    signals, create user profile automatically when new user is created

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
@@ -18,7 +19,7 @@ class Lead(models.Model):
 	last_name = models.CharField(max_length=20)
 	age = models.IntegerField(default=0)
 	agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
-	
+
 
 class Agent(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -26,3 +27,12 @@ class Agent(models.Model):
 
 	def __str__(self):
 		return self.user.email
+
+
+def post_user_created_signal(sender, instance, created, **kwargs):
+	print(instance, created)
+	if created:
+		UserProfile.objects.create(user=instance)
+
+
+post_save.connect(post_user_created_signal, sender=User)
