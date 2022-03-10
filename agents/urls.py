4dba201628724fa86f245b934c42dc9601860baf
Jisoo <jisoooh0202@gmail.com>
2022-03-10 23:02:41 +0700

    add agent update

--- agents/urls.py
+++ agents/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AgentDetailView, AgentListView, AgentCreateView
+from .views import AgentDetailView, AgentListView, AgentCreateView, AgentUpdateView
 
 
 app_name = 'agents'
@@ -7,5 +7,6 @@ app_name = 'agents'
 urlpatterns = [
 	path('', AgentListView.as_view(), name='agent-list'),
 	path('create/', AgentCreateView.as_view(), name='agent-create'),
-	path('<pk>/', AgentDetailView.as_view(), name='agent-detail')
+	path('<pk>/', AgentDetailView.as_view(), name='agent-detail'),
+	path('<pk>/update/', AgentUpdateView.as_view(), name='agent-update'),
 ]
