d673a98182cfed7c4f30e5339bd1697d4fff7b05
Jisoo <jisoooh0202@gmail.com>
2022-03-18 01:23:21 +0700

    add category list view

--- leads/models.py
+++ leads/models.py
@@ -34,6 +34,7 @@ class Agent(models.Model):
 
 class Category(models.Model):
 	name = models.CharField(max_length=30) # New, Contacted, Converted, Unconverted
+	organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.name
--- leads/urls.py
+++ leads/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import (
 	lead_list, lead_detail, lead_create, lead_update, lead_delete,
-	LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView, AssignAgentView
+	LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView, AssignAgentView, CategoryListView
 )
 
 
@@ -10,6 +10,7 @@ app_name = "leads"
 urlpatterns = [
 	path('', LeadListView.as_view(), name='lead-list'),
 	path('create/', LeadCreateView.as_view(), name='lead-create'),
+	path('categories/', CategoryListView.as_view(), name='category-list'),
 	path('<pk>/', LeadDetailView.as_view(), name='lead-detail'),
 	path('<pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
 	path('<pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
